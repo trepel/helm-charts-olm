88b8e406ca397e1195049660724c32deb43fb52d
Alex Zgabur <azgabur@redhat.com>
2024-10-02 15:24:38 +0200

    Add ossm, refactor everything, add tools disable

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Quick script to run helm
 
-echo "Installing operators" && \
+echo "---Installing operators---" && \
 helm install --values values.yaml --values additionalManifests.yaml --wait kuadrant-operators operators/ && \
-echo "\nInstalling instances" && \
+echo -e "\n---Installing instances---" && \
 helm install --values values.yaml --values additionalManifests.yaml --wait kuadrant-instances instances/ && \
+./hack.sh && \
 echo "Success!"
-
