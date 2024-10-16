826c7fb133282143f9ce9871662f04c981e1b22f
Alex Zgabur <azgabur@redhat.com>
2024-10-16 16:12:24 +0200

    Add CRD uninstall step, only specify startingCSV if Manual

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-# Quick script to uninstall helm
+# Quick script to uninstall helm and kuadrant CRD
 
 echo "Uninstalling instances" && \
 helm uninstall --wait kuadrant-instances
 echo "Uninstalling operators" && \
 helm uninstall --wait kuadrant-operators && \
 kubectl delete ns cert-manager && \
+kubectl get crd -o name | grep "kuadrant" | xargs kubectl delete && \
 echo "Success!"
 
