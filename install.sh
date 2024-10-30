9ac372fb80fee79809768cf7a8a401756b6efbc8
Alex Zgabur <azgabur@redhat.com>
2024-10-30 13:30:54 +0100

    Add fix for running scripts outside of git directory

--- install.sh
+++ install.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 # Quick script to run helm
 
+cd "$(dirname "$0")" || exit 1
+
 IMAGE="${IMAGE:-quay.io/kuadrant/kuadrant-operator-catalog:nightly-$(date +%d-%m-%Y)}"
 CHANNEL="${CHANNEL:-preview}"
 DEPLOY_TESTSUITE="${DEPLOY_TESTSUITE:-true}"
 
-if [ "$1" = "-i" ]; then
-    printf "What kuadrant image you want?: "
-    read -r IMAGE
-
-    printf "What channel you want? 'preview' for nightly, 'stable' for releases: "
-    read -r CHANNEL
-
-    printf "Do you want to deploy testsuite environment? 'true' or 'false': "
-    read -r DEPLOY_TESTSUITE
-fi
-
 if [ "$1" = "-t" ]; then
-    echo "Installing with image: $IMAGE channel $CHANNEL"
+    echo "Installing channel $CHANNEL with image: $IMAGE"
     echo "Testsuite deploy: $DEPLOY_TESTSUITE"
     echo "---Installing operators---" && \
     helm install \
@@ -39,6 +30,7 @@ if [ "$1" = "-t" ]; then
     ./hack.sh && \
     echo "Success!"
 else
+    echo "Using defaults from values.yaml"
     echo "Installing operators" && \
     helm install --values values.yaml --wait kuadrant-operators operators/ && \
     echo "Installing instances" && \
--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # Quick script to uninstall helm and kuadrant CRD
 
-echo "Uninstalling instances" && \
+cd "$(dirname "$0")" || exit 1
+
+echo "Uninstalling instances"
 helm uninstall --ignore-not-found --wait kuadrant-instances
-echo "Uninstalling operators" && \
+echo "Uninstalling operators"
 helm uninstall --ignore-not-found --wait kuadrant-operators && \
 kubectl delete --ignore-not-found ns cert-manager && \
 kubectl get crd -o name | grep "kuadrant" | xargs --no-run-if-empty kubectl delete && \
