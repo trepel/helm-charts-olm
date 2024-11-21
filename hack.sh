85a7fbd90d5163de5a70680ea0164068d9b044b2
Alex Zgabur <azgabur@redhat.com>
2024-11-21 15:06:06 +0100

    Different hack for Keycloak 26

--- hack.sh
+++ hack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+USER=$(kubectl get -n tools secret keycloak-initial-admin --template="{{.data.username}}" | base64 -d)
 PASS=$(kubectl get -n tools secret keycloak-initial-admin --template="{{.data.password}}" | base64 -d)
-kubectl create -n tools secret generic credential-sso --from-literal=ADMIN_USERNAME=admin --from-literal="ADMIN_PASSWORD=$PASS"
+kubectl create -n tools secret generic credential-sso --from-literal="ADMIN_USERNAME=$USER" --from-literal="ADMIN_PASSWORD=$PASS"
 
