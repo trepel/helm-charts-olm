76360ab4e64fd837f2e5b3c6beda2d8715bea236
Alex Zgabur <azgabur@redhat.com>
2025-03-10 13:59:29 +0100

    Expand uninstall script with deletion of kuadrant objects

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,11 @@
 # Quick script to uninstall helm and kuadrant CRD
 
 cd "$(dirname "$0")" || exit 1
+echo "Removing leftover Kuadrant objects"
+for crd in $(kubectl get crd -o name | grep "kuadrant" | sed 's/.*\/\(.*\)/\1/'); do
+    kubectl get --chunk-size=0 -o name -n "kuadrant" "$crd" |\
+    xargs --no-run-if-empty -P 20 -n 1 kubectl delete --ignore-not-found -n "kuadrant"
+done
 
 echo "Uninstalling instances"
 helm uninstall --ignore-not-found --wait kuadrant-instances
